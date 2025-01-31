7afb82cd63d301b99a06a1d13d8407e42eb4c6fe
NathanBitoun <nathanbitoun@gmail.com>
2025-01-31 17:32:25 +0100

    on s ameliore

--- ex3/index.php
+++ ex3/index.php
@@ -13,8 +13,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Vulnérabilité SQLi détectable par un DAST mais pas par Semgrep
-    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    // Contournement de la détection Semgrep : Utilisation de LIKE
+    $sql = "SELECT * FROM users WHERE username LIKE '$username' AND password LIKE '$password'";
     $result = $conn->query($sql);
 
     if ($result && $result->num_rows > 0) {
